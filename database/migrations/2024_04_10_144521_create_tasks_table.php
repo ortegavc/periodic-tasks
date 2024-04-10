df189905ae6aa76b636b578e5b0a7b957de8a229
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2024-04-10 10:18:50 -0500

    Define mingrations and seeders.

---
+++ database/migrations/2024_04_10_144521_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->date('due_date')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->unsignedTinyInteger('group_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
--- database/seeders/GroupSeeder.php
+++ database/seeders/GroupSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class GroupSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class GroupSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('groups')->insert([
+            ['name' => 'Bugifx', 'description' => 'Used for work fixing product bugs', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Feature', 'description' => 'Used for work on new features.', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Hotfix', 'description' => 'Used for fixing urgent bugs on production that have high impact.', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Release', 'description' => 'Used for preparing and testing your work for release.', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 }
