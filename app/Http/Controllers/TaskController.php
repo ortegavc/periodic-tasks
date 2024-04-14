6b4528343f3e0bf2e319894746fc10e218e4bee9
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2024-04-14 14:13:53 -0500

    List tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,7 +15,13 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return view('tasks/index');
+        $pending_tasks = Task::where('completed', false)->orderBy('due_date')->get(['id', 'title', 'description', 'due_date']);
+        $today = $pending_tasks->where('due_date', now()->format('Y-m-d'));
+        $tomorrow = $pending_tasks->where('due_date', now()->addDay()->format('Y-m-d'));
+        $nextWeek = $pending_tasks->whereBetween('due_date', [now()->nextWeekday()->subDay(), now()->nextWeekendDay()]);
+        $nearFuture = $pending_tasks->whereBetween('due_date', [now()->nextWeekendDay(), now()->nextWeekendDay()->addDays(7)]);
+        $farFuture =  $pending_tasks->where('due_date', '>', now()->nextWeekendDay()->addDays(7));
+        return view('tasks/index', compact('today', 'tomorrow', 'nextWeek', 'nearFuture', 'farFuture'));
     }
 
     /**
